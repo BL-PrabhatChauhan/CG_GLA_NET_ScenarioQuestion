7c04af51f923bbe8f54e5d9493b1a1bf578856a1
bl-chandrashekar <chandrashekar.kalmood@bridgelabz.com>
2025-05-21 10:42:33 +0530

    feat(Intial Commit):Added Questions 1 to 4 from PDF 1

---
+++ Sahil/StudentattendanceTracker/StudentattendanceTracker/StudentTracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentattendanceTracker
+{
+    public class StudentTracker
+    {
+        public Dictionary<String,int> attendancePerDay(List<List<string>> attendance)
+        {
+            Dictionary<String,int> students = new Dictionary<String,int>();
+
+            foreach (var studentInThatDay in attendance)
+            {
+                foreach (var student in studentInThatDay) {
+                    if (students.ContainsKey(student))
+                    {
+                        students[student]++;
+                    }
+                    else
+                    {
+                        students[student] = 1;
+                    }
+                }
+            }
+            return students;
+        }
+    }
+}
